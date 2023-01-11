3c31bf5384cf94e86a1de7e04c8e5f5c11a76d7f
Adrien GRALL <adrien.grall2020@campus-eni.fr>
2023-01-11 10:09:03 +0100

    suppression dossier inutile

--- Autostock/src/main/java/fr/eni/autostock/dal/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.eni.autostock.dal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.eni.autostock.bo.Car;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-
-}
--- Autostock/src/main/java/fr/eni/autostock/dal/EnergyRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.eni.autostock.dal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.eni.autostock.bo.Energy;
-
-public interface EnergyRepository extends JpaRepository<Energy, Long> {
-
-}
